5b230d5eebb7b0148b4bc0e6f98ac516ae07fef5
Nuri1977 <nurilacka@gmail.com>
2022-01-11 17:46:41 +0100

    Remove static data from html

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ const renderList = () => {
   for (let i = 0; i < todos.length; i += 1) {
     const content = `
     <li class="list-item">
-      <input type="checkbox" id="vehicle1" name="vehicle1" value=${todos[i].completed}>
+      <input type="checkbox" name="vehicle1" value=${todos[i].completed}>
       <h4 class="todo-item">${todos[i].description}</h4>
       <i id=${todos[i].index} class="fas fa-trash-alt"></i>
     </li>
